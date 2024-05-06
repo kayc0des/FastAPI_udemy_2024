abfaa99ed07ac807bfdb797554f00d14c438d2fd
Kingsley B. Boafo <109430532+kayc0des@users.noreply.github.com>
2024-05-06 08:39:39 +0200

    list assignment

---
+++ assignments/24_assignment.py
@@ -0,0 +1,19 @@
+''' Python Lists '''
+
+# create a list of 5 animals called zoo
+zoo = ['lion', 'monkey', 'crocodile', 'tiger', 'black mamba']
+
+# delete the animal at the 3rd Index
+zoo.pop(2)
+
+# append a new animal to the end of the list
+zoo.append('antelope')
+
+# Delete the animal at the begining of the list
+zoo.pop(0)
+
+# print all the animals
+print(zoo)
+
+# print only the first three
+print(zoo[:3])
