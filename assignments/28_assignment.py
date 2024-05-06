9b74908be7f1c2b2baa168c90281154deee481e2
Kingsley B. Boafo <109430532+kayc0des@users.noreply.github.com>
2024-05-06 08:46:41 +0200

    if else

---
+++ assignments/28_assignment.py
@@ -0,0 +1,15 @@
+''' If Else Statements '''
+
+# create a variable grade holding an integer between 0-100
+grade = 65
+
+if 90 <= grade <= 100:
+    print('A')
+elif 80 <= grade <= 89:
+    print('B')
+elif  70 <= grade <= 79:
+    print('C')
+elif 60 <= grade <= 69:
+    print('D')
+else:
+    print('F') 
